c2274a1c78aba5d2f8e58d10822734aad313639a
Yangjing Zhang <zhangyangjing@gmail.com>
2016-11-10 17:24:05 +0800

    add line chart

--- app/src/main/java/com/zhangyangjing/weather/ui/fragment/FragmentDaily.java
+++ app/src/main/java/com/zhangyangjing/weather/ui/fragment/FragmentDaily.java
@@ -1,22 +1,162 @@
 package com.zhangyangjing.weather.ui.fragment;
 
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.zhangyangjing.weather.R;
+import com.zhangyangjing.weather.provider.weather.WeatherContract.WeatherDaily;
+import com.zhangyangjing.weather.settings.SettingsUtil;
+import com.zhangyangjing.weather.ui.widget.LineChartView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by zhangyangjing on 05/11/2016.
  */
 
 public class FragmentDaily extends Fragment {
+    private static final String TAG = FragmentDaily.class.getSimpleName();
+
+    private static final int LOADER_ID = 0;
+
+    private MyAdapter mAdapter;
+    private MyLoaderCallback mLoaderCallback;
+
+    @BindView(R.id.rv_daily) RecyclerView mRvDaily;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new MyAdapter();
+        mLoaderCallback = new MyLoaderCallback();
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_daily, container, false);
+        Log.d(TAG, "onCreateView() called with: inflater = [" + inflater + "], container = [" + container + "], savedInstanceState = [" + savedInstanceState + "]");
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        getLoaderManager().initLoader(LOADER_ID, null, mLoaderCallback);
+        MatrixCursor cursor = new MatrixCursor(new String[]{"low", "high"});
+        cursor.addRow(new Integer[]{0, 3});
+        cursor.addRow(new Integer[]{3, 6});
+        cursor.addRow(new Integer[]{4, 6});
+        cursor.addRow(new Integer[]{6, 9});
+        cursor.addRow(new Integer[]{5, 9});
+        cursor.addRow(new Integer[]{2, 4});
+        cursor.addRow(new Integer[]{4, 7});
+        cursor.addRow(new Integer[]{9, 12});
+        mRvDaily.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        mRvDaily.setAdapter(mAdapter);
+        mAdapter.swapCursor(cursor);
+    }
+
+    private class MyLoaderCallback implements LoaderManager.LoaderCallbacks {
+
+        @Override
+        public Loader onCreateLoader(int id, Bundle args) {
+            return new CursorLoader(
+                    getContext(),
+                    WeatherDaily.buildObserveUri(SettingsUtil.getCurrentCity(getContext())),
+                    null, null, null, null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader loader, Object data) {
+            mAdapter.swapCursor((Cursor) data);
+        }
+
+        @Override
+        public void onLoaderReset(Loader loader) {
+            mAdapter.swapCursor(null);
+        }
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        private Cursor mCursor;
+        private int mMaxTemp;
+        private int mMinTemp;
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = View.inflate(parent.getContext(), R.layout.item_daily, null);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            int temp[] = getTemp(position);
+            int nextTemp[] = mCursor.getCount() - 1 == position ? temp : getTemp(position + 1);
+            int previousTemp[] = 0 == position ? temp : getTemp(position - 1);
+
+//            holder.mTvInfo.setText("");
+            holder.mLcChart.setData(
+                    mMaxTemp,
+                    mMinTemp,
+                    new int[] {previousTemp[0], temp[0], nextTemp[0]},
+                    new int[] {previousTemp[1], temp[1], nextTemp[1]});
+        }
+
+        @Override
+        public int getItemCount() {
+            return null == mCursor ? 0 : mCursor.getCount();
+        }
+
+        public void swapCursor(Cursor cursor) {
+            mCursor = cursor;
+            updateLimit();
+            notifyDataSetChanged();
+        }
+
+        private int[] getTemp(int index) {
+            mCursor.moveToPosition(index);
+            return new int[] {
+                    mCursor.getInt(mCursor.getColumnIndex("low")),
+                    mCursor.getInt(mCursor.getColumnIndex("high"))};
+        }
+
+        private void updateLimit() {
+            mMinTemp = Integer.MAX_VALUE;
+            mMaxTemp = Integer.MIN_VALUE;
+
+            for (int i = 0; i < mCursor.getCount(); i++) {
+                int[] temp = getTemp(i);
+                if (temp[0] < mMinTemp)
+                    mMinTemp = temp[0];
+                if (temp[1] > mMaxTemp)
+                    mMaxTemp = temp[1];
+            }
+        }
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv1) TextView mTvInfo;
+        @BindView(R.id.lc_chart) LineChartView mLcChart;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 }
---
+++ app/src/main/java/com/zhangyangjing/weather/ui/widget/LineChartView.java
@@ -0,0 +1,110 @@
+package com.zhangyangjing.weather.ui.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by zhangyangjing on 10/11/2016.
+ */
+
+public class LineChartView extends View {
+    private static final String FORMAT_STRING = "%d°";
+    private static final int STROKE_WIDTH = 6;
+
+    private int[] mLowData, mHighData;
+    private int mMax, mMin;
+
+    Paint mLinePaint;
+    Paint mTextPaint;
+    Paint mShadePaint;
+
+    public LineChartView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mLinePaint = new Paint();
+        mLinePaint.setAntiAlias(true);
+        mLinePaint.setStrokeWidth(STROKE_WIDTH);
+        mLinePaint.setStyle(Paint.Style.STROKE);
+
+        mTextPaint = new Paint();
+        mTextPaint.setTypeface(Typeface.createFromAsset(getContext().getAssets(),
+                "Oswald-Regular.ttf")); // TODO: use util
+        mTextPaint.setTextSize(40);
+
+        mShadePaint = new Paint();
+    }
+
+    public void setData(int max, int min, int[] lowData, int[] highData) {
+        mMax = max;
+        mMin = min;
+        mLowData = lowData;
+        mHighData = highData;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawColor(0xffeeeeee);
+
+        mLinePaint.setColor(0xFFFE872F);
+        mShadePaint.setShader(new LinearGradient(
+                0, 0, 0, getHeight(),
+                new int[]{0xFFFEE6CE, 0x00FFFFFF},
+                null, Shader.TileMode.REPEAT));
+        drawChart(canvas, mHighData, true);
+
+        mLinePaint.setColor(0xFF2EB0E9);
+        mShadePaint.setShader(new LinearGradient(
+                0, 0, 0, getHeight(),
+                new int[]{0xFFABDBEF, 0x00FFFFFF},
+                null, Shader.TileMode.CLAMP));
+        drawChart(canvas, mLowData, false);
+    }
+
+    private void drawChart(Canvas canvas, int[] data, boolean textUpward) {
+        Rect rect = new Rect();
+        String info = String.format(FORMAT_STRING, data[1]);
+        mTextPaint.getTextBounds(info, 0, info.length(), rect);
+        int textWidth = rect.right - rect.left;
+        int textHeight = rect.bottom - rect.top;
+
+        int rangeTop = textHeight + STROKE_WIDTH * 2;
+        int rangeBottom = getHeight() - textHeight - STROKE_WIDTH * 2;
+
+        int verticalRange = rangeBottom - rangeTop;
+        float rate = verticalRange / (mMax - mMin);
+
+        Point previous = new Point(-getWidth() / 2, (int) (rangeBottom - (data[0] - mMin) * rate));
+        Point current = new Point(getWidth() / 2, (int) (rangeBottom - (data[1] - mMin) * rate));
+        Point next = new Point(getWidth() * 3 / 2, (int) (rangeBottom - (data[2] - mMin) * rate));
+
+        Path path = new Path();
+        path.moveTo(previous.x, previous.y);
+        path.cubicTo(0, previous.y, 0, current.y, current.x, current.y);
+        path.cubicTo(getWidth(), current.y, getWidth(), next.y, next.x, next.y);
+
+        Path shadePath = new Path();
+        shadePath.addPath(path);
+        shadePath.lineTo(getWidth(), getHeight());
+        shadePath.lineTo(0, getHeight());
+
+        canvas.drawPath(shadePath, mShadePaint);
+        canvas.drawPath(path, mLinePaint);
+        canvas.drawCircle(current.x, current.y, STROKE_WIDTH, mLinePaint);
+
+        int textY = textUpward ?
+                current.y - STROKE_WIDTH * 2 :
+                current.y + STROKE_WIDTH * 2 + textHeight;
+        canvas.drawText(info, current.x - textWidth / 2, textY, mTextPaint);
+    }
+}
